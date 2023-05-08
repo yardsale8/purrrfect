465e0f9d005ce8e6835d3d5ddb2b86eb82b69de5
Todd Iverson <Tiverson@winona.edu>
2023-05-08 08:18:42 -0500

    Refactored num_successes
    Now num_successes requires a map, but num_successes_int abstracts the map away and returns an int column.

--- R/outcome_helpers.R
+++ R/outcome_helpers.R
@@ -1,6 +1,8 @@
 #' Counting Successes
 #'
 #' Count the number of success for a given value or predicate function.
+#' * `num_success()` always returns a list column and should be `map`ped over outcomes.  Can be composed with other function/expressions inside a map.
+#' * `num_success_int()`always returns an integer column and does not need to be `map`ped. Useful in creating a new integer column.
 #'
 #'
 #' @param col A list of length 1 containing a single outcome, either an atomic element or vector.
@@ -16,34 +18,65 @@
 #' # Make sure the outcome is stored in list of length 1
 #' one.outcome <- list(c('H', 'H', 'T', 'H', 'T'))
 #'
-#' # Comparing to a atomic chr
-#' one.outcome %>% num_successes('H')
+#' # Comparing to a atomic chr, returning a list
+#' one.outcome %>% map(\(x) num_successes(x, 'H'))
 #'
-#' # walk(print) to inspect intermediate results
-#' one.outcome %>% walk(print) %>% num_successes('H')
+#' # Comparing to a atomic chr, returning a integer vector
+#' one.outcome %>% map_int(\(x) num_successes(x, 'H'))
+#'
+#' # Same as above, simplified
+#' one.outcome %>% num_successes_int('H')
+#'
+#' # Output is a list vector w/ same length as input list
+#' one.outcome %>% map(\(x) num_successes(x, 'H'))
 #'
 #' # Output is a integer vector w/ same length as input list
-#' one.outcome %>% num_successes('H') %>% str
+#' one.outcome %>% num_successes_int('H') %>% str
+#'
+#' # walk(print) to inspect intermediate results
+#' one.outcome %>% walk(print) %>% num_successes_int('H')
 #'
 #' # Compare using a predicate function
-#'  one.outcome %>% num_successes(\(x) x != 'H')
+#' one.outcome %>% map_int(\(x) num_successes(x, \(t) t != 'H'))
+#' one.outcome %>% num_successes_int(\(x) x != 'H')
 #'
 #' # Use `map` to generate a list of multiple outcomes
 #' coin <- c('H', 'T')
 #' outcomes <- map(1:5, \(i) sample(coin, 3, replace=TRUE))
+#' outcomes
 #'
 #' # Comparing to a atomic chr
-#' outcomes%>% num_successes('H')
+#' outcomes%>% num_successes_int('H')
 #'
 #' # walk(print) to inspect intermediate result
-#' outcomes %>% walk(print) %>% num_successes('H')
+#' outcomes %>% walk(print) %>% num_successes_int('H')
+#'
+#' # Output is a list vector w/ same length as input list
+#' outcomes %>% map(\(x) num_successes(x, 'H')) %>% str
 #'
 #' # Output is a integer vector w/ same length as input list
-#' outcomes %>% num_successes('H') %>% str
+#' outcomes %>% num_successes_int('H') %>% str
 #'
 #' # Compare using a predicate function
-#'  outcomes %>% num_successes(\(x) x != 'H')
+#'  outcomes %>% map_int(\(x) num_successes(x, \(t) t != 'H'))
+#'  outcomes %>% num_successes_int(\(x) x != 'H')
+#'
+#' # Composition.
+#' # Use num_successes to, e.g., ask questions about the number of successes.
+#' outcomes %>% walk(str) %>%  map_lgl(\(x) num_successes(x, 'H') > 1)
 num_successes <-
+  function(outcome, .p)
+  {
+    f <- if (rlang::is_function(.p)) {
+      .p
+    } else {
+      function(x) x == .p
+    }
+    length(purrr::keep(outcome, f))
+  }
+#' @rdname num_successes
+#' @export
+num_successes_int <-
   function(col, .p)
   {
     f <- if (rlang::is_function(.p)) {
@@ -52,7 +85,6 @@ num_successes <-
       function(x) x == .p
     }
     (col
-      %>% purrr::modify(f)
-      %>% purrr::map_int(~purrr::reduce(.x, `+`, .init = 0))
+      %>% purrr::map_int(\(x) num_successes(x, f))
     )
   }
--- tests/testthat/test-outcome_helpers.R
+++ tests/testthat/test-outcome_helpers.R
@@ -1,22 +1,28 @@
-test_that("Atomic objects work", {
-  outcomes <- list(c('H', 'H', 'T', 'H', 'T'))
-  expect_equal(num_successes(outcomes, 'H'), 3)
-  all_tails <- list(rep_len('T', 5))
-  expect_equal(num_successes(all_tails, 'H'), 0)
-  expect_equal(num_successes(all_tails, 'T'), 5)
+test_that("atomic objects work", {
+  outcomes <- list(c('h', 'h', 't', 'h', 't'))
+  expect_equal(num_successes_int(outcomes, 'h'), 3)
+  expect_equal(purrr::map_int(outcomes, \(x) num_successes(x, 'h')), 3)
+  all_tails <- list(rep_len('t', 5))
+  expect_equal(num_successes_int(all_tails, 'h'), 0)
+  expect_equal(num_successes_int(all_tails, 't'), 5)
+  expect_equal(purrr::map_int(all_tails, \(x) num_successes(x, 'h')), 0)
+  expect_equal(purrr::map_int(all_tails, \(x) num_successes(x, 't')), 5)
 })
 
 test_that("predicate functions work", {
   outcomes <- list(c('H', 'H', 'T', 'H', 'T'))
-  expect_equal(num_successes(outcomes, \(x) x != 'H'), 2)
+  expect_equal(num_successes_int(outcomes, \(x) x != 'H'), 2)
+  expect_equal(purrr::map_int(outcomes, \(x) num_successes(x, \(t) t != 'H')), 2)
   all_tails <- list(rep_len('T', 5))
-  expect_equal(num_successes(all_tails, \(x) x == 'H'), 0)
-  expect_equal(num_successes(all_tails, \(x) x != 'H'), 5)
+  expect_equal(num_successes_int(all_tails, \(x) x == 'H'), 0)
+  expect_equal(num_successes_int(all_tails, \(x) x != 'H'), 5)
+  expect_equal(purrr::map_int(all_tails, \(x) num_successes(x, \(t) t == 'H')), 0)
+  expect_equal(purrr::map_int(all_tails, \(x) num_successes(x, \(t) t != 'H')), 5)
 })
 
 test_that("Works on empty vectors", {
   empty <- list(c())
-  expect_equal(num_successes(empty, 'H'), 0)
-  expect_equal(num_successes(empty, \(x) x != 'H'), 0)
+  expect_equal(num_successes_int(empty, 'H'), 0)
+  expect_equal(num_successes_int(empty, \(x) x != 'H'), 0)
 })
 
