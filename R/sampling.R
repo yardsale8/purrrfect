7bd6aee89889105e406a8b7607f41d7cb4156e5a
Todd Iverson <Tiverson@winona.edu>
2023-05-11 07:09:04 -0500

    Added sampling function for waiting time problems
    sample_until will perform both sampling with and without replacement and looks for either one atomic match or a match of the provided predicate function.

--- R/sampling.R
+++ R/sampling.R
@@ -9,4 +9,102 @@
 take.one.sample <-
   function(x, size, replace = FALSE, prob=NULL) {
     list(sample(x, size, replace, prob))
+  }
+
+get_success_func <-
+  function(.p) {
+    .f <- if (rlang::is_function(.p)) {
+      .p
+    } else {
+      function(x) sum(x == .p) == 1
     }
+    return(.f)
+  }
+
+sample_until_replace <-
+  function(x, .p, prob = NULL, halt = 1000)
+  {
+    .f <- get_success_func(.p)
+    out <- base::sample(x, size=1, prob = prob, replace=TRUE)
+    n <- 1
+
+    while(!.f(out) & n < halt) {
+      n <- n + 1
+      out[n] <- base::sample(x, size=1, prob = prob, replace=TRUE)
+    }
+
+    return(out)
+  }
+
+sample_until_no_replace <-
+  function(x, .p, prob = NULL) {
+    .f <- get_success_func(.p)
+    perm <- base::sample(x, replace = FALSE, prob = prob)
+    halt <- length(x)
+    n <- 1
+    out <- perm[1:n]
+
+    while(!.f(out) & n < halt) {
+      n <- n + 1
+      out <- perm[1:n]
+    }
+    return(out)
+  }
+
+#' Sample until a given condition
+#'
+#' Take a sample until meeting some user-provided condition is met.
+#'
+#' @param x Either a vector of one more more elements, or a positive integer.
+#' @param .p Either an atomic element or a Boolean predicate function.
+#' @param replace Whether to sample with replacement.
+#' @param prob a vector of weigths for obtaining the element of `x`
+#' @param halt an integer limiting the length of the output.  Will be set to `length(x)` when `replace=FALSE`
+#'
+#' @return The smallest sample that either (A) matches `.p` when `.p` is atomic, (B) satisfies the predicate `.p`, (C) has reached the halting length.
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#'
+#' # Flipping a fair coin
+#' coin <- c('H', 'T')
+#' sample_until(coin, 'H', replace = FALSE)
+#' first.head <- replicate(5, sample_until(coin, 'H', replace = TRUE))
+#' first.head %>% glimpse
+#' three.heads <- \(x) num_successes(x, 'H') == 3
+#' third.head <- replicate(5, sample_until(coin, three.heads, replace = TRUE))
+#' third.head %>% glimpse
+#'
+#' # Flipping a biased coin
+#' coin <- c('H', 'T')
+#' prob <- c(0.6, 0.4)
+#' sample_until(coin, 'H', replace = FALSE, prob=prob)
+#' first.head <- replicate(5, sample_until(coin, 'H', replace = TRUE, prob=prob))
+#' first.head %>% glimpse
+#' three.heads <- \(x) num_successes(x, 'H') == 3
+#' third.head <- replicate(5, sample_until(coin, three.heads, replace = TRUE, prob=prob))
+#' third.head %>% glimpse
+#'
+#' # Sampling without replacement
+#' urn <- c(rep_len('Blue', 10), rep_len('White', 5))
+#' urn
+#'
+#' sample_until(urn, 'Blue', replace=FALSE)
+#'
+#' first.blue <- replicate(5, sample_until(urn, 'Blue', replace=FALSE))
+#' first.blue %>% glimpse
+#'
+#' second.blue <- replicate(5, sample_until(urn, \(x) num_successes(x, 'Blue') == 2, replace=FALSE))
+#' second.blue %>% glimpse
+sample_until <-
+  function(x, .p, replace = FALSE, prob = NULL, halt = 1000) {
+    if (replace) {
+      return(sample_until_replace(x, .p, prob, halt))
+
+    } else {
+      return(sample_until_no_replace(x, .p, prob))
+    }
+  }
+
+
--- tests/testthat/test-sampling.R
+++ tests/testthat/test-sampling.R
@@ -14,3 +14,31 @@ test_that("sample without replacement and default size returns whole x", {
   expect_length(take.one.sample(x), 1)
   expect_length(take.one.sample(x)[[1]], 2)
 })
+
+test_that("sample until with replacement and atomic element works", {
+  x <- c('H', 'T')
+  actual <- sample_until(x, 'H', replace = TRUE)
+  expect_true(all(num_successes(x, 'H') == 1))
+})
+
+test_that("sample until without replacement and atomic element works", {
+  x <- c('H', 'T')
+  actual <- sample_until(x, 'H', replace = FALSE)
+  expect_true(all(num_successes(actual, 'H') == 1))
+  expect_true(all(length(x) >= length(actual)))
+})
+
+test_that("sample until with replacement and predicate works", {
+  x <- c('B', 'B', 'B', 'W', 'W')
+  f <- \(x) num_successes(x, 'B') == 2
+  actual <- sample_until(x, f, replace = TRUE)
+  expect_true(all(num_successes(actual, 'B') == 2))
+})
+
+test_that("sample until without replacement and predicate works", {
+  x <- c('B', 'B', 'B', 'W', 'W')
+  f <- \(x) num_successes(x, 'B') == 2
+  actual <- sample_until(x, f, replace = FALSE)
+  expect_true(all(num_successes(actual, 'B') == 2))
+  expect_true(all(length(x) >= length(actual)))
+})
